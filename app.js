83b06b893921643f0264cbb8588355edd27713d9
Bart Kalanski <bkalanski@gmail.com>
2020-05-25 19:58:11 +0100

    changed colour of output box

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ document.getElementById('tip-form').onchange = function(){
     let tipAmount = bringTipInfo.options[bringTipInfo.selectedIndex].text;
     let totalTip = tipAmount / 100 + 1;
     let totalAmount = Math.round(totalTip * billAmount);
-    document.getElementById('total').innerHTML = `Your bill is £${billAmount}<br>Your tip is ${tipAmount}%<br>Your total amount to pay is £${totalAmount}<br>`;
+    document.getElementById('total').innerHTML = `Price: £${billAmount}<br>Tip: ${tipAmount}%<br>Total: £${totalAmount}<br>`;
     document.getElementById('total').style.display='block';
 }
 
