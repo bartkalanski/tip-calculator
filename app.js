9bd93240a10d6c41db4eec7b9091486c63413d7e
Bart <61667381+bkalan011@users.noreply.github.com>
2020-06-09 09:41:26 +0100

    Added split amount functionality

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-
-document.getElementById('tip-form').onchange = function(){
+document.getElementById('tip-form').onchange = function() {
     let billAmount = Number(document.getElementById('bill-amount').value);
     let bringTipInfo = document.getElementById('service-rating');
+    let splitAmount = Number(document.getElementById('split-amount').value);
     let tipAmount = bringTipInfo.options[bringTipInfo.selectedIndex].text;
     let totalTip = tipAmount / 100 + 1;
-    let totalAmount = Math.round(totalTip * billAmount);
-    document.getElementById('total').innerHTML = `Price: £${billAmount}<br>Tip: ${tipAmount}%<br>Total: £${totalAmount}<br>`;
-    document.getElementById('total').style.display='block';
+    let totalSplitAmount = Math.round((totalTip * billAmount) / splitAmount);
+    let totalAmount = totalSplitAmount * splitAmount
+    document.getElementById('total').innerHTML = `Price: £${billAmount}<br>Tip: ${tipAmount}%<br>Split per person: £${totalSplitAmount}<br>Total: £${totalAmount}<br>`;
+    document.getElementById('total').style.display = 'block';
 }
-
-
